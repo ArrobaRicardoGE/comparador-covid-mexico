1ad1fae6926d7148261c830526c901ec9a042f17
ArrobaRicardoGE <ricardoarroba@outlook.com>
2020-12-15 14:09:39 -0600

    Implementar la clase para la interfaz de usuario. Problemas conocidos: es muy lento

--- CSVManager.java
+++ CSVManager.java
@@ -10,19 +10,21 @@ import java.util.ArrayList;
 
 class CSVManager{
     public static ArrayList<String[]> read(String filename) throws IOException, FileNotFoundException{
+        System.out.println("In");
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename),"utf-8"));
         String line;
         ArrayList<String[]> contents = new ArrayList<>();
+        System.out.println("In");
         while((line = br.readLine()) != null)
             contents.add(line.split(","));
         br.close();
         return contents;
     }
-    public static void clearFile(String filename) throws IOException, FileNotFoundException{
-        FileOutputStream clear = new FileOutputStream(filename);
-        clear.close();
-    }
-    public static void writeRow(String filename,String[] row) throws IOException, FileNotFoundException{
+    public static void writeRow(String filename,String[] row, boolean clear) throws IOException, FileNotFoundException{
+        if(clear){
+            FileOutputStream clearFile = new FileOutputStream(filename);
+            clearFile.close();
+        }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
             new FileOutputStream(filename,true),"utf-8"));
         boolean first = true;
