10d86ac5c6981330b4ba501265c347968dc4ceca
ArrobaRicardoGE <ricardoarroba@outlook.com>
2020-12-19 17:25:22 -0600

    Incluir el modulo del comparador

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import PySimpleGUI as sg
-import util
+import util,os
 
-progress_bar = sg.ProgressBar(100,size=(25,10),visible=False,metadata={
-    'progress':0,
-    'status':None
-})
+progress_bar = sg.ProgressBar(100,size=(25,10),visible=False)
+
 messages = sg.Text(visible=False,size=(25,1),justification='center')
 
 layout = [
@@ -23,9 +21,13 @@ while True:
         if values['-FILE1-'] == util.gui_constants.FILE_PLACEHOLDER or values['-FILE2-'] == util.gui_constants.FILE_PLACEHOLDER:
             messages.update(value=util.gui_constants.FILE_NOT_SELECTED,text_color='#FF0000',visible=True)
         else:
+            messages.update(value=util.gui_constants.READING_FILE.format(os.path.basename(values['-FILE1-'])),text_color='#0000FF',visible=True)
+            progress_bar.update(0,visible=True)
+            messages.update(value=util.gui_constants.READING_FILE.format(os.path.basename(values['-FILE2-'])),text_color='#0000FF',visible=True)
+            progress_bar.update(25,visible=True)
             messages.update(value=util.gui_constants.GENERATING,text_color='#0000FF',visible=True)
-            for i in range(100):
-                progress_bar.update(current_count=i, visible=True) 
+            progress_bar.update(50,visible=True)
+
     if event == sg.WIN_CLOSED:
         break
 
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 class gui_constants:
     FILE_PLACEHOLDER = 'Selecciona el archivo...'
     FILE_NOT_SELECTED = 'Selecciona ambos archivos'
+    READING_FILE = 'Leyendo archivo {}'
     GENERATING = 'Generando...'
