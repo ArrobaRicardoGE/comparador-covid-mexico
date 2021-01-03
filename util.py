9a17815e10d17fbaf98a1f25210ae68090f19fb8
ArrobaRicardoGE <ricardoarroba@outlook.com>
2021-01-02 23:52:53 -0600

    Agregar primera version del notebook

--- util.py
+++
@@ -1,5 +0,0 @@
-class gui_constants:
-    FILE_PLACEHOLDER = 'Selecciona el archivo...'
-    FILE_NOT_SELECTED = 'Selecciona ambos archivos'
-    READING_FILE = 'Leyendo archivo {}'
-    GENERATING = 'Generando...'
